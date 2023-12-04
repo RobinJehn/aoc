f7e4b9430a51b4d8a257f63c0caf65b2060fc8ec
Jonathan Good <github@bungogood.com>
2023-12-04 16:39:58 +0000

    Tidying 2023 1st - 3rd

--- 2023/day_01/day_01.py
+++ 2023/day_01/day_01.py
@@ -1,32 +1,18 @@
-def part1():
+def part1(data):
     nums = []
-    with open("input.txt") as f:
-        data = f.read().splitlines()
-        for line in data:
-            first = None
-            last = None
-            for char in line:
-                if char.isdigit():
-                    if first is None:
-                        first = char
-                    last = char
-            nums.append(int(first + last))
+    for line in data.splitlines():
+        first = None
+        last = None
+        for char in line:
+            if char.isdigit():
+                if first is None:
+                    first = char
+                last = char
+        nums.append(int(first + last))
     return sum(nums)
 
-
-def part2():
+def part2(data):
     nums = []
-    digit_strings = [
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-    ]
     to_digit = {
         "one": "1",
         "two": "2",
@@ -38,28 +24,29 @@ def part2():
         "eight": "8",
         "nine": "9",
     }
-    with open("input.txt") as f:
-        data = f.read().splitlines()
-        for line in data:
-            first = None
-            last = None
-            for idx in range(len(line)):
-                num = None
-                if line[idx].isdigit():
-                    num = line[idx]
-                for digit in digit_strings:
-                    if line[idx:].startswith(digit):
-                        num = to_digit[digit]
-                        break
+    for line in data.splitlines():
+        first = None
+        last = None
+        for idx in range(len(line)):
+            num = None
+            if line[idx].isdigit():
+                num = line[idx]
+            for (key, val) in to_digit.items():
+                if line[idx:].startswith(key):
+                    num = val
+                    break
 
-                if num is not None:
-                    if first is None:
-                        first = num
-                    last = num
-            nums.append(int(first + last))
+            if num is not None:
+                if first is None:
+                    first = num
+                last = num
+        nums.append(int(first + last))
     return sum(nums)
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    file = "input.txt"
+    with open(file) as f:
+        data = f.read()
+        print(part1(data))
+        print(part2(data))
