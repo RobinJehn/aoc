b26b20c1be15af16eafee1f4af4cd74e2c81837a
RobinJehn <robin.jehn@gmx.de>
2024-12-01 18:18:15 +0000

    day_01

---
+++ 2024/fetch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Usage: ./fetch.sh year [day]
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+fi
+
+# Check if SESSION variable is set
+if [ -z "$SESSION" ]; then
+    echo "Error: SESSION variable not set. Aborting."
+    exit 1
+fi
+
+# Function to download input for a given day
+download_day() {
+    YEAR=$1
+    DAY=$(printf "%02d" $2)  # Pad day with leading zero
+    URL="https://adventofcode.com/$YEAR/day/${DAY#0}/input"  # Remove leading zero for URL
+    DIR="inputs"
+    FILE="$DIR/day_$DAY.txt"
+
+    # Check if the required directories exist, create if not
+    if [ ! -d "$DIR" ]; then
+        mkdir -p "$DIR"
+    fi
+
+    if curl -s -b "session=$SESSION" "$URL" --output "$FILE"; then
+        echo "Downloaded $YEAR Day ${DAY#0} to $FILE"
+    else
+        echo "Failed $YEAR Day ${DAY#0}"
+    fi
+}
+
+# Download data for all days or a single day
+if [ -z "$2" ]; then
+    for DAY in {1..25}; do
+        download_day $1 $DAY
+    done
+else
+    download_day $1 $2
+fi
--- 2024/src/bin/day_01.rs
+++ 2024/src/bin/day_01.rs
@@ -1,3 +1,63 @@
+use std::collections::HashMap;
+use std::{fs, vec};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "inputs/day_01.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let mut vec_1 = vec![];
+    let mut vec_2 = vec![];
+    for line in contents.lines() {
+        let mut iter = line.split_whitespace();
+        let first = iter.next().unwrap();
+        let second = iter.next().unwrap();
+
+        let first_num: i32 = first.parse().expect("Failed to parse");
+        let second_num: i32 = second.parse().expect("Failed to parse");
+
+        vec_1.push(first_num);
+        vec_2.push(second_num);
+    }
+
+    vec_1.sort();
+    vec_2.sort();
+
+    let total_diff = part1(&vec_1, &vec_2);
+
+    println!("Total diff: {}", total_diff);
+
+    let similarity_score = part2(&vec_1, &vec_2);
+
+    println!("Similarity score: {}", similarity_score);
+}
+
+fn part1(vec_1: &Vec<i32>, vec_2: &Vec<i32>) -> i32 {
+    let mut total_diff = 0;
+    for (a, b) in vec_1.iter().zip(vec_2.iter()) {
+        total_diff += (a - b).abs();
+    }
+
+    total_diff
+}
+
+fn part2(vec_1: &Vec<i32>, vec_2: &Vec<i32>) -> i32 {
+    let mut number_of_appearences = HashMap::new();
+
+    for num in vec_2.iter() {
+        if let Some(count) = number_of_appearences.get(num) {
+            number_of_appearences.insert(num, count + 1);
+        } else {
+            number_of_appearences.insert(num, 1);
+        }
+    }
+
+    let mut similarity_score = 0;
+
+    for num in vec_1.iter() {
+        if let Some(count) = number_of_appearences.get(num) {
+            similarity_score += count * num;
+        }
+    }
+
+    similarity_score
 }
